dcad51cacbbeaf7b7f2e53af4292364aac02c596
dylanlinux <accounts@dylankirdahy.nyc>
2022-05-10 17:56:33 -0400

    SPI in working - further testing necessary

--- pwm_module.v
+++ pwm_module.v
@@ -1,24 +1,43 @@
-module pwm_module (input wire clk,
+module pwm_module (input wire pwm_clk, chip_select, mosi, sclk, parallel_enable,
 						 input wire [7:0] pwm_duty_in,
 						 output reg pwm_out);
-	integer pwm_duty, counter;
+						 
+	reg [7:0] pwm_duty_out, pwm_duty, pwm_counter, spi_in;
+	reg [2:0] spi_counter;
+	reg spi_begin;	
 	
-	initial
+	always @ (posedge sclk)
 		begin
-			counter = 0;
+			if (chip_select)
+				spi_counter <= 0;
+			else
+				spi_in[spi_counter] <= mosi;
+				spi_counter <= spi_counter + 1;
+		end	
+		
+	always @ (spi_begin, pwm_duty_in, parallel_enable)
+		begin
+			if(parallel_enable)
+				pwm_duty <= pwm_duty_in;
+			else
+				if(chip_select)
+					pwm_duty <= spi_in;
 		end
 
-	always @ (posedge clk)
+	always @ (posedge pwm_clk)
 		begin
-			if (counter <= pwm_duty)
+			if (pwm_counter < pwm_duty_out)
 				pwm_out <= 1;
 			else
 				pwm_out <= 0;
 				
-		counter = counter + 1;
-		if (counter == 255)
-			counter <= 0;
-			pwm_duty <= pwm_duty_in;
+		if (pwm_counter == 0)
+			pwm_duty_out <= pwm_duty;
+			
+		pwm_counter <= pwm_counter + 1;
+		
+		if(pwm_counter == 254) //Necessary so that 255 makes it constantly high
+			pwm_counter <= 0;
 		end
 		
 endmodule
