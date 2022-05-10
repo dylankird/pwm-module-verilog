886f00c8d5104d998f03a5910bfc2aa6e916abae
Dylan Kirdahy <me@dylankirdahy.nyc>
2022-05-10 14:29:22 -0400

    minor improvements

--- pwm_module.v
+++ pwm_module.v
@@ -1,8 +1,24 @@
-module pwm_module (input wire ext_clk,
+module pwm_module (input wire clk,
+						 input wire [7:0] pwm_duty_in,
 						 output reg pwm_out);
-	reg int_clk;
+	integer pwm_duty, counter;
 	
-	always
-		#1 int_clk = ~int_clk;
+	initial
+		begin
+			counter = 0;
+		end
+
+	always @ (posedge clk)
+		begin
+			if (counter <= pwm_duty)
+				pwm_out <= 1;
+			else
+				pwm_out <= 0;
+				
+		counter = counter + 1;
+		if (counter == 255)
+			counter <= 0;
+			pwm_duty <= pwm_duty_in;
+		end
 		
 endmodule
