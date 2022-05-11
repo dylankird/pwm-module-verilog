49fb9dba7510b21ed5efc1c2b23ef60b55252dc3
Dylan Kirdahy <me@dylankirdahy.nyc>
2022-05-10 21:47:20 -0400

    Everything fully working (just need to add comments)

--- pwm_module.v
+++ pwm_module.v
@@ -4,18 +4,19 @@ module pwm_module (input wire pwm_clk, chip_select, mosi, sclk, parallel_enable,
 						 
 	reg [7:0] pwm_duty_out, pwm_duty, pwm_counter, spi_in;
 	reg [2:0] spi_counter;
-	reg spi_begin;	
 	
-	always @ (posedge sclk)
+	always @ (posedge sclk, posedge chip_select)
 		begin
 			if (chip_select)
 				spi_counter <= 0;
 			else
-				spi_in[spi_counter] <= mosi;
-				spi_counter <= spi_counter + 1;
+				begin
+					spi_in[spi_counter] <= mosi;
+					spi_counter <= spi_counter + 1;
+				end
 		end	
 		
-	always @ (spi_begin, pwm_duty_in, parallel_enable)
+	always @ *
 		begin
 			if(parallel_enable)
 				pwm_duty <= pwm_duty_in;
